bf25df45f37ead9d106aebf9d425ff1aca5d8163
Kovan <kovan.hu@gmail.com>
2024-05-25 09:27:00 +0100

    serving notice

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ def main():
     print("Learning Coffee Menu - in Progress")
     print("Asking coffee maker to get me a coffee")
     Make_coffee()
-
-
+    print("Coffee Served")
 if __name__ == "__main__":
     main()
